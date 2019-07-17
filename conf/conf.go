fc9e95f46b2252cde711d440905e884c028708ef
qlu1990 <qlu.19900128@163.com>
2019-07-17 21:01:57 +0800

    go.mod

--- conf/conf.go
+++ conf/conf.go
@@ -8,22 +8,28 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+//Mongodb struct  mongodb conf
 type Mongodb struct {
 	Host     string `yaml:"host"`
 	User     string `yaml:"user"`
 	Password string `yaml:"password"`
 }
 
+//Server struct server mongodb conf
 type Server struct {
 	Address string `yaml:"address"`
 }
+
+//Conf struct for conf file
 type Conf struct {
 	Mongodb Mongodb `yaml:"mongodb"`
 	Server  Server  `yaml:"server"`
 }
 
+//Cfg global var conf
 var Cfg Conf
 
+//LoadConf load conf file
 func LoadConf() {
 	workdir, err := os.Getwd()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.12
 require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-cmp v0.3.0 // indirect
 	github.com/qlu1990/gos v0.0.0-20190715141640-611d2a176e27
+	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/tidwall/pretty v1.0.0 // indirect
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.0.4
